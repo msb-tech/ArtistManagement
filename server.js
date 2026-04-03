c4b3a470f178a5c9197df6564bfb3bd52e2c7e81
msb <baghelmahendrasingh24@gmail.com>
2026-04-03 23:49:25 +0530

    Demo Get URL Test

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.get("/api/test", (req, res) => {
+  res.send("API is working 🚀");
+});
 
 // Routes
 const artistRoutes = require('./routes/artistRoutes');
